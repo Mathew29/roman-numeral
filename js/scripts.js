1150a65fb1081b4c43eff8b3bdead789c78d333d
Darrion Gering and Mathew Akre <mathew@>
2019-04-04 13:52:27 -0700

    took away unnecessary variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-var I = 1;
-var V = 5;
-var X = 10;
-var L = 50;
-var C = 100;
-var D = 500;
-var M = 1000;
-
-
 function conversion(number) {
   var numArray = turnNumberToArray(number)
   if (number > 3999) {
